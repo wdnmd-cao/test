dc0a687b53cfc41928569d581e38c3ad71273f80
wdnmd-cao <75286006+wdnmd-cao@users.noreply.github.com>
2022-01-15 20:04:25 +0800

    Create 1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,18 @@
+#include <windows.h>
 #include <stdio.h>
 int main(void)
 {
-  
+  for(;;)
+  {
+    int a;
+    scanf_s("%d",&a);
+    printf("enter a to quit");
+    if(GetAsyncKeyboard(a))
+    {
+      break;
+    }
+  }
+  printf("%d\n",a);
   printf("hello");
   return 0;
 }
